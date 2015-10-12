99635006f6bd2fb649f447a7c252bc24ec864f16
rochness <rochelle.y.lee@gmail.com>
2015-10-12 15:56:13 -0700

    Solve findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,29 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // make array of length n where each element is an array of length n (all zeros)
+  var solution = [];
+
+  for (var i = 0; i < n; i++) {
+    solution.push([]);
+    for (var j = 0; j < n; j++) {
+      solution[i].push(0);
+    }
+  }
+  var columnsCalled = [];
+
+  // iterate through each row
+  for (var rowIndex = 0; rowIndex < n; rowIndex++) {
+    // call Math.random (somewhere between 0 and n-1), set this to colIndex
+    var colIndex = Math.floor(Math.random() * n);
+    while (columnsCalled.indexOf(colIndex) !== -1) {
+      colIndex = Math.floor(Math.random() * n);
+    }
+    columnsCalled.push(colIndex);
+    // set that point in the matrix equal to 1
+    solution[rowIndex][colIndex] = 1;
+  }
+
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
